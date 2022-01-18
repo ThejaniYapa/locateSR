b73a6d8377554f1df1509246df99bbd7e7e9af45
ThejaniYapa <thejyapa95@gmail.com>
2022-01-18 19:03:23 +0530

    markers created

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
 /*
- * Copyright 2019 Google LLC. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+demo- 
  */
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
@@ -21,13 +9,23 @@ import "./style.css";
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
 let map: google.maps.Map, infoWindow: google.maps.InfoWindow;
+let markers: google.maps.Marker[] = [];
+let latlng:google.maps.LatLng
 
 function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
+    center: { lat: 7.5270786, lng: 79.86124 },
+    zoom: 10,
   });
-  infoWindow = new google.maps.InfoWindow();
+
+  infoWindow = new google.maps.InfoWindow({position: map.getCenter()});
+
+  let bannerWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Latitude and Longitude",
+    //position: { lat: 7.5270786, lng: 79.86124 },
+    position: map.getCenter(),
+  });
+  bannerWindow.open(map);
 
   const locationButton = document.createElement("button");
   locationButton.textContent = "Pan to Current Location";
@@ -37,8 +35,23 @@ function initMap(): void {
   submitButton.textContent = "Submit Your Location";
   submitButton.classList.add("custom-map-control-button");
 
+  const markerButton = document.createElement("button");
+  markerButton.textContent = "Mark the Location";
+  markerButton.classList.add("custom-map-control-button");
+
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
   map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(submitButton);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(markerButton);
+
+  map.addListener("click", (mapsMouseEvent) => {
+    //marker.setMap(null);
+    setMapOnAll(null);
+    bannerWindow.close();
+    bannerWindow = new google.maps.InfoWindow({position: mapsMouseEvent.latLng,});
+    bannerWindow.setContent(JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2));
+    bannerWindow.open({map,shouldFocus: false,});
+    placeMarkerAndPanTo(mapsMouseEvent.latLng, map);
+  });
 
   locationButton.addEventListener("click", () => {
     // Try HTML5 geolocation.
@@ -49,11 +62,15 @@ function initMap(): void {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
-
-          infoWindow.setPosition(pos);
+          //currentLoc= new google.maps.LatLng(-34.397, 150.644),
+          infoWindow.close();
           infoWindow.setContent("Location found: "+ pos.lat+"," +pos.lng);
-          infoWindow.open(map);
+          infoWindow.setPosition(pos)
+          infoWindow.open({map,shouldFocus: false,});
           map.setCenter(pos);
+          const marker = new google.maps.Marker({position: pos,map: map,});
+          markers.push(marker);
+          //placeMarkerAndPanTo(pos, map);
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter()!);
@@ -66,28 +83,14 @@ function initMap(): void {
   });
 
   submitButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position: GeolocationPosition) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
+    infoWindow.setContent("Location found...");
+    infoWindow.open(map);
+  });
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter()!);
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter()!);
-    }
+  markerButton.addEventListener("click", () => {
+    //infoWindow.setPosition();
+    infoWindow.setContent("Location found...");
+    infoWindow.open(map);
   });
 
 }
@@ -105,4 +108,22 @@ function handleLocationError(
   );
   infoWindow.open(map);
 }
+
+function placeMarkerAndPanTo(latLng: google.maps.LatLng, map: google.maps.Map) {
+  const marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+  });
+  map.panTo(latLng);
+  markers.push(marker);
+}
 export { initMap };
+
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map: google.maps.Map | null) {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+  markers = [];
+}
